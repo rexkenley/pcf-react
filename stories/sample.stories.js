833364c91b0f7f4b8ea8721e5049d202a46b8cc9
Rex <rexkenley@hotmail.com>
2020-06-15 11:10:05 -0400

    update

--- stories/sample.stories.js
+++ stories/sample.stories.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { initializeIcons } from "@uifabric/icons";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
-storiesOf("Sample", module)
-  .add("Initial", () => {});
+initializeIcons();
+
+storiesOf("Sample", module).add("Initial", () => {});
